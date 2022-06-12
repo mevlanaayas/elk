5bc1c6116b512f2461e08836a8844517a2c287b4
mevlanaayas <mevlanaayas@gmail.com>
2022-06-12 16:43:01 +0300

    added: common field

---
+++ src/main/java/com/mevl/elk/UserIdHandlerInterceptor.java
@@ -0,0 +1,24 @@
+package com.mevl.elk;
+
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Configuration
+public class UserIdHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MDC.put("user_id", UUID.randomUUID().toString());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        MDC.remove("user_id");
+    }
+}
